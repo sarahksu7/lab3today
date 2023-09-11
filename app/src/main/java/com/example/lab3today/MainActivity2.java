26d2631d9d4d2cea7dc017161d42ce06ed4513f6
sarahksu7 <sma123908@gmail.com>
2023-09-11 11:10:41 +0300

    lab3

--- app/src/main/java/com/example/lab3today/MainActivity2.java
+++ app/src/main/java/com/example/lab3today/MainActivity2.java
@@ -2,13 +2,21 @@ package com.example.lab3today;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
-
+TextView receiver_msg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        receiver_msg= (TextView) findViewById(R.id.received_value_id);
+        Intent intent = getIntent();
+        String str = intent.getStringExtra("message_key");
+        receiver_msg.setText("Hello, "+str+"!");
+
+
     }
 }
